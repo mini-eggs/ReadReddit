ff22b63a2d8d4b0fee67d992e4cd15c21a86206e
Evan <evan@crowdhubapps.com>
2016-09-11 14:37:04 -0700

    fixing rest api usage

--- src/methods/index.js
+++ src/methods/index.js
@@ -3,6 +3,10 @@ const Entities = require('html-entities').AllHtmlEntities;
 
 const entities = new Entities();
 
+const aesjs = require('aes-js');
+
+const encryptionKey = require("../creds/encryption.js");
+
 /**
  *
  * @param scriptSource
@@ -136,7 +140,47 @@ const getSubredditBySearch = function(Obj){
 };
 export{getSubredditBySearch}
 
+/**
+ *
+ * @param htmlWithEntities
+ */
+
 const getDecodedHtml = function(htmlWithEntities){
     return entities.decode(htmlWithEntities);
 };
 export{getDecodedHtml}
+
+/**
+ *
+ * @param Obj
+ */
+
+const request = function(Obj){
+
+    let url = window.location.href;
+    let arr = url.split("/");
+    let resultUrl = arr[0] + "//" + arr[2];
+    let toGet = resultUrl + Obj.url;
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            let toReturn;
+            if(this.responseText.length > 0) {
+                toReturn = JSON.parse(this.responseText);
+            }
+            Obj.onComplete(toReturn);
+        }
+    };
+    xhttp.open("GET", toGet, true);
+    xhttp.send();
+};
+export{request}
+
+const decrypt = function(text){
+    var key = aesjs.util.convertStringToBytes(encryptionKey);
+    var aesCtr = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(5));
+    var decryptedBytes = aesCtr.decrypt(text);
+    return aesjs.util.convertBytesToString(decryptedBytes);
+};
+export{decrypt}
--- src/store/index.js
+++ src/store/index.js
@@ -3,18 +3,9 @@ import Vue from 'vue';
 
 import Vuex from 'vuex';
 
+import { request, decrypt } from '../methods';
+
 const Fingerprint2 = require('fingerprintjs2');
-
-const aesjs = require('aes-js');
-
-const encryptionKey = require("../creds/encryption.js");
-
-const decrypt = function(text){
-    var key = aesjs.util.convertStringToBytes(encryptionKey);
-    var aesCtr = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(5));
-    var decryptedBytes = aesCtr.decrypt(text);
-    return aesjs.util.convertBytesToString(decryptedBytes);
-};
 
 Vue.use(Vuex);
 
@@ -34,115 +25,66 @@ const store = new Vuex.Store({
         },
         FETCH_UPVOTE: ({ commit, dispatch, state }, Obj) => {
             return new Promise(function(resolve, reject){
-
-                let url = window.location.href;
-                let arr = url.split("/");
-                let resultUrl = arr[0] + "//" + arr[2];
-                let toGet = resultUrl + '/api/upvote/?token=' + Obj.token + '&content=' + Obj.content + '&score=' + Obj.score;
-
-                var xhttp = new XMLHttpRequest();
-                xhttp.onreadystatechange = function() {
-                    if (this.readyState == 4 && this.status == 200) {
-                        let curr = JSON.parse(this.responseText);
-                        resolve(curr);
-                    }
-                };
-                xhttp.open("GET", toGet, true);
-                xhttp.send();
+                request({
+                    url:'/api/upvote/?token=' + Obj.token + '&content=' + Obj.content + '&score=' + Obj.score,
+                    onComplete:function(curr){resolve(curr);}
+                });
             })
         },
         FETCH_LOGOUT: ({ commit, dispatch, state }, data) => {
-
             new Fingerprint2().get(function(deviceHash, components){
-
-                let url = window.location.href;
-                let arr = url.split("/");
-                let resultUrl = arr[0] + "//" + arr[2];
-                let toGet = resultUrl + '/api/sessionDestroy/?device=' + deviceHash;
-
-                var xhttp = new XMLHttpRequest();
-                xhttp.open("GET", toGet, true);
-                xhttp.send();
-
-                commit('SET_LOGIN', {null});
+                request({
+                    url:'/api/sessionDestroy/?device=' + deviceHash,
+                    onComplete:function(curr){}
+                });
+                commit('SET_LOGIN', {null});//we don't need to wait on the request
             });
         },
         FETCH_LOGIN_SESSION: ({ commit, dispatch, state }, data) => {
-
             new Fingerprint2().get(function(deviceHash, components){
-
-                let url = window.location.href;
-                let arr = url.split("/");
-                let resultUrl = arr[0] + "//" + arr[2];
-                let toGet = resultUrl + '/api/sessionUsers/?device=' + deviceHash;
-
-                var xhttp = new XMLHttpRequest();
-                xhttp.onreadystatechange = function() {
-                    if (this.readyState == 4 && this.status == 200) {
-                        if(this.responseText.length > 0) {
-                            let curr = JSON.parse(this.responseText);
+                request({
+                    url:'/api/sessionUsers/?device=' + deviceHash,
+                    onComplete:function(curr){
+                        if(typeof curr == 'object') {
                             curr['status'] = true;
                             curr.p = decrypt(JSON.parse(curr.p));
                             commit("SET_SESSION_LOGIN", {curr});
                         } else {
-                            let curr = {};
+                            curr = {};
                             curr['status'] = false;
                             commit("SET_SESSION_LOGIN", {curr});
                         }
                     }
-                };
-                xhttp.open("GET", toGet, true);
-                xhttp.send();
+                });
             });
         },
         FETCH_LOGIN_ASYNC: ({ commit, dispatch, state }, data) => {
-
             new Fingerprint2().get(function(deviceHash, components){
-
-                let url = window.location.href;
-                let arr = url.split("/");
-                let resultUrl = arr[0] + "//" + arr[2];
-                let toGet = resultUrl + '/api/login/?username=' + data.username + '&password=' + data.password + '&device=' + deviceHash;
-
-                var xhttp = new XMLHttpRequest();
-                xhttp.onreadystatechange = function() {
-                    if (this.readyState == 4 && this.status == 200) {
-                        let curr = JSON.parse(this.responseText);
+                request({
+                    url:'/api/login/?username=' + data.username + '&password=' + data.password + '&device=' + deviceHash,
+                    onComplete:function(curr){
                         if(curr.data) {
-
                             curr['status'] = {status:true};
                             curr.password = decrypt(curr.password);
-
                             if(!(curr.token.length > 0)){
                                 var verifyUrl = "https://www.reddit.com/api/v1/authorize?client_id=Cd9I1cNvacR6Vw&response_type=code&state=" + curr.device + "&redirect_uri=http://reddit.evanjon.es/&duration=permanent&scope=vote";
                                 location.href = verifyUrl;
                             }
-
                             commit('SET_LOGIN', {curr});
                         } else { //logins incorrect
                             curr = {status:false, message:'Creds incorrect'};
                             commit('SET_LOGIN', {curr});
                         }
                     }
-                };
-                xhttp.open("GET", toGet, true);
-                xhttp.send();
+                });
             });
         }
     },
     mutations: {
-        SET_MESSAGE_USER: (state, { curr }) => {
-            state.messageUser = curr;
-        },
-        SET_VISITED: (state, { curr }) => {
-            state.visited = curr;
-        },
-        SET_LOGIN: (state, { curr }) => {
-            state.user = curr;
-        },
-        SET_SESSION_LOGIN: (state, { curr }) => {
-            state.sessionUser = curr;
-        }
+        SET_MESSAGE_USER: (state, { curr }) => {state.messageUser = curr;},
+        SET_VISITED: (state, { curr }) => {state.visited = curr;},
+        SET_LOGIN: (state, { curr }) => {state.user = curr;},
+        SET_SESSION_LOGIN: (state, { curr }) => {state.sessionUser = curr;}
     },
     getters: {
         getSessionUser (state) {
